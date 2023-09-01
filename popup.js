aa1fec57f8ebf1f9a9bf6245e7316c49e946839e
Sharan Singh <sharan.singh0203@gmail.com>
2023-08-31 23:57:15 -0700

    attractions are displaying

---
+++ popup.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchButton = document.getElementById('searchButton');
+    const cityInput = document.getElementById('cityInput');
+    const resultsDiv = document.getElementById('results');
+
+    searchButton.addEventListener('click', function () {
+        const city = cityInput.value;
+        if (city) {
+            searchCity(city);
+        }
+    });
+
+    async function searchCity(city) {
+        const apiKey = '78314c7265msha48978f37f3a697p1b00a2jsn1a4165569374';
+        const searchUrl = `https://opentripmap-places-v1.p.rapidapi.com/en/places/geoname?name=${city}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': apiKey,
+                'X-RapidAPI-Host': 'opentripmap-places-v1.p.rapidapi.com'
+            }
+        };
+        try {
+            const response = await fetch(searchUrl, options);
+            const searchData = await response.json();
+            const latitude = searchData.lat;
+            const longitude = searchData.lon;
+            console.log('longitude', longitude);
+            // Call a function to fetch attractions using the latitude and longitude
+            fetchAttractions(latitude, longitude);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    async function fetchAttractions(lat, lon) {
+        const apiKey = '78314c7265msha48978f37f3a697p1b00a2jsn1a4165569374';
+        const attractionsUrl = `https://opentripmap-places-v1.p.rapidapi.com/en/places/radius?radius=500&lon=${lon}&lat=${lat}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': apiKey,
+                'X-RapidAPI-Host': 'opentripmap-places-v1.p.rapidapi.com'
+            }
+        };
+        try {
+            const response = await fetch(attractionsUrl, options);
+            const attractionsData = await response.json();
+            
+              // Log the retrieved API response data
+            console.log('API Response Data:', attractionsData);
+
+            // Process and display attractions data
+            displayAttractions(attractionsData);
+        } catch (error) {
+            console.error('Error fetching attractions:', error);
+        }
+    }
+
+    function displayAttractions(data) {
+        const resultsDiv = document.getElementById('results');
+        
+        // Clear previous results
+        resultsDiv.innerHTML = '';
+        
+        if (data && data.features && data.features.length > 0) {
+            const attractionsList = document.createElement('ul');
+    
+            data.features.forEach(feature => {
+                const attractionName = feature.properties.name;
+                const attractionElement = document.createElement('li');
+                attractionElement.textContent = attractionName;
+                attractionsList.appendChild(attractionElement);
+            });
+    
+            resultsDiv.appendChild(attractionsList);
+        } else {
+            const noResultsMessage = document.createElement('p');
+            noResultsMessage.textContent = 'No attractions found near this city.';
+            resultsDiv.appendChild(noResultsMessage);
+        }
+    }
+    
+});
